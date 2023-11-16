a957d0a285530cf0c2fbd918a02c86470e82f05a
Saad ERRAOUI <saaderraouiinfosup@gmail.com>
2023-11-16 08:51:42 +0100

    PUSH-FTPRINTF:1.1.1 Update ft_putptr.c file

--- ft_putptr.c
+++ ft_putptr.c
@@ -6,19 +6,11 @@
 /*   By: serraoui <serraoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 17:43:46 by serraoui          #+#    #+#             */
-/*   Updated: 2023/11/15 17:58:33 by serraoui         ###   ########.fr       */
+/*   Updated: 2023/11/15 23:45:20 by serraoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-// #include <stdio.h>
-// #include <unistd.h>
-
-// void	ft_putchar(char c, int *len)
-// {
-// 	write(1, &c, 1);
-//     (*len)++;
-// }
 
 void    ft_putptr(unsigned long number, int *len)
 {
@@ -38,13 +30,3 @@ void    ft_putptr(unsigned long number, int *len)
 			ft_putchar(ft_tolower((number % 16) + '7'), len);
 	}
 }
-
-// int main()
-// {
-// 	int a = 0;
-// 	int b = -1;
-// 	int *c = &a;
-// 	ft_putptr(15, c);
-// 	c = &b;
-// 	printf("\n%d\n", a);
-// }
